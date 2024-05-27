a8aefa84d32d17e8a0473628f9ec0848ff84b3e2
Konstantin Kot <sigankov.06@mail.ru>
2024-05-27 11:57:42 +0500

    Added student table rendering

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
     APIHost: 'http//api.school.local',
-    classesURL: 'http://api.school.local/api/schoolclasses'
+    classesURL: 'http://api.school.local/api/schoolclasses',
+    studentsURL: 'http://api.school.local/api/students',
+    teachersURL: 'http://api.school.local/api/teachers',
 }
--- src/views/ClassesView.vue
+++ src/views/ClassesView.vue
@@ -1,27 +1,78 @@
-<script setup>
+<script setup xmlns="http://www.w3.org/1999/html">
 import PageHeader from "@/components/PageHeader.vue";
 import axios from 'axios'
 import config from '@/config.js'
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 
 const classes = reactive([])
-
+const students = reactive([])
+const teacher = reactive([])
+const classID = ref('')
+const className = ref('')
 axios.get(config.classesURL).then(function(response){
   for (const schoolClass of response.data) {
     classes.push(schoolClass)
   }
-  console.log(classes)
 })
+function submitClassSelectionForm(emit) {
+  emit.preventDefault()
+  console.log(classID.value)
+  axios.get(config.classesURL+'/'+classID.value+'/withStudentsAndTeachers')
+      .then(function (response){
+        for (const student of response.data.students) {
+          students.push(student)
+        }
+
+        teacher.value=response.data.teacher
+        console.log(teacher.value)
+        className.value=response.data.name
+      })
+}
 </script>
 
 <template>
   <PageHeader></PageHeader>
-  <form>
-    <label for="classes">Выберите класс</label>
-    <select id="classes" name="classes"  v-if="classes.length" >
-      <option :value="schoolClass.name" v-for="schoolClass in classes">{{schoolClass.name}}</option>
+  <form class="d-flex justify-content-center my-4" @submit="submitClassSelectionForm">
+    <label for="classes" class="mx-2">Выберите класс</label>
+    <select id="classes" name="classes"  v-model='classID' v-if="classes.length" >
+      <option :value="schoolClass.id" v-for="schoolClass in classes">{{schoolClass.name}}</option>
     </select>
+    <input type="submit" class="btn btn-primary mt-2 mx-2" value="Отобразить список класса">
   </form>
+  <h3 class="text-center">Ученики {{className}}</h3>
+  <h5 class="text-center" v-if="teacher.value.name">Классный руководитель - {{teacher.value.name}}</h5>
+  <section v-if="students.length" class="container">
+    <table class="my-4 table table-striped">
+      <tr>
+        <th>
+          Фамилия
+        </th>
+        <th>
+          Имя
+        </th>
+        <th>
+          Отчество
+        </th>
+        <th>
+          День рождения
+        </th>
+      </tr>
+      <tr v-for="student in students">
+        <td>
+          {{student.surname}}
+        </td>
+        <td>
+          {{student.first_name}}
+        </td>
+        <td>
+          {{student.last_name}}
+        </td>
+        <td>
+          {{student.birthday}}
+        </td>
+      </tr>
+    </table>
+  </section>
 </template>
 
 <style scoped>
